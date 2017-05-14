308aed1361648043a714e5830067a4061c7ee0f8
Arwa-M <Arwa-M@users.noreply.github.com>
2017-05-14 22:36:41 +0100

    Update asteroids.cpp
    comments added

--- asteroids/src/asteroids.cpp
+++ asteroids/src/asteroids.cpp
@@ -1,7 +1,6 @@
-/* Asteroids
-    Sample solution for assignment
-    Semester 2 -- Small Embedded Systems
-    Dr Alun Moon
+/* Asteroids Assignment
+Student Name: Arwa Mhannee
+Student ID: 15007722		
 */
 
 /* C libraries */
@@ -24,47 +23,59 @@
 float elapsed_time; 
 int   score;
 int   lives;
+int 	shields;
 struct ship player;
 
 float Dt = 0.01f;
 
 Ticker model, view, controller;
 
-bool paused = true;
+bool paused = false;
+bool mode = false;
+bool gameEnd = false;
+bool game = false;
+
+void initialiseShip(ship){
+	player.p.x = 250;
+	player.p.y = 100;
+	player.shields = 3;
+}
+
 /* The single user button needs to have the PullUp resistor enabled */
 DigitalIn userbutton(P2_10,PullUp);
 int main()
 {
-
-    init_DBuffer();
-    
-
-    view.attach( draw, 0.025);
-    model.attach( physics, Dt);
-    controller.attach( controls, 0.1);
-    
-    lives = 5;
-    
-    /* Pause to start */
-    while( userbutton.read() ){ /* remember 1 is not pressed */
-        paused=true;
-        wait_ms(100);
-    }
-    paused = false;
-    
-    while(true) {
-        /* do one of */
-        /* Wait until all lives have been used
-        while(lives>0){
-            // possibly do something game related here
-            wait_ms(200);
-        }
-        */
-        /* Wait until each life is lost
-        while( inPlay ){
-            // possibly do something game related here
-            wait_ms(200);
-        }
-        */
-    }
+	init_DBuffer();
+	
+	view.attach( draw, 0.025);
+	model.attach( physics, Dt);
+	controller.attach( controls, 0.1);
+	
+	lives = 5;
+	score = 0;
+	shields = 3;
+	elapsed_time = 0;
+	
+	/* Press to start */
+	while( userbutton.read() ){ /* remember 1 is not pressed */
+			mode = false;
+			wait_ms(100);
+	}
+	mode = true;
+	
+	while(mode == true) {
+		game = true;
+		while(lives >0){
+			initialiseShip(player);
+		}
+		/* When each life is lost detach all tickers and initate gameover screen*/
+		if(lives==0 && shields==0){
+			gameEnd = true;
+			game = false;
+			wait_ms(200);
+			view.detach();
+			model.detach();
+			controller.detach();
+		}	
+	}
 }
