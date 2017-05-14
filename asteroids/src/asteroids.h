230b643b62e34dfc9764202d6af5cd8c9a47e4e3
Arwa-M <Arwa-M@users.noreply.github.com>
2017-05-14 22:37:53 +0100

    Update asteroids.h
    comments added

--- asteroids/src/asteroids.h
+++ asteroids/src/asteroids.h
@@ -3,6 +3,11 @@
 extern float elapsed_time; /* time this ship has been active */
 extern int   score;        /* total score so far */
 extern int   lives;        /* lives remaining */
+extern int   shields;      /* shields remaining */
+extern bool  paused;			 /* game is paused */
+extern bool  gameEnd;			 /* game is Over */
+extern bool  game;			 	 /* game state */
+extern bool  mode;			 	 /* mode of game */
 
 extern struct ship player;
 
